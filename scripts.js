c30476d27af42ddbe9f17e818076d1c1a9a24fca
Faizaan <fdatoo7@gmail.com>
2020-05-04 01:46:49 -0400

    More screenies

--- scripts.js
+++ scripts.js
@@ -1,1 +1,4 @@
-var images = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg', 'images/5.jpg', 'images/6.jpg', 'images/7.jpg']; $.backstretch(images, { duration: 5000, fade: 750 });
+var images = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg', 'images/5.jpg', 'images/6.jpg', 'images/7.jpg',
+    'images/8.jpg', 'images/9.jpg', 'images/10.jpg', 'images/11.jpg', 'images/12.jpg'];
+
+$.backstretch(images, { duration: 5000, fade: 750 });
