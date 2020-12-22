084909b08ecc3479502e84a5ed417a5c55b55c31
Faizaan Datoo <fdatoo7@gmail.com>
2020-12-22 17:29:17 -0500

    Added map embed to the page

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
 var images = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg', 'images/5.jpg', 'images/6.jpg', 'images/7.jpg',
     'images/8.jpg', 'images/9.jpg', 'images/10.jpg', 'images/11.jpg', 'images/12.jpg'];
 
-$.backstretch(images, { duration: 5000, fade: 750 });
+$("#landing").backstretch(images, { duration: 5000, fade: 750 });
+
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+        licenseKey: '',
+        navigation: true,
+        scrollBar: true
+	});
+});
