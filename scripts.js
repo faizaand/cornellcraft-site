cba1aede6ba28dd409537075c63a950c72fd8567
Faizaan Datoo <fdatoo7@gmail.com>
2020-12-22 20:25:09 -0500

    New images

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-var images = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg', 'images/5.jpg', 'images/6.jpg', 'images/7.jpg',
-    'images/8.jpg', 'images/9.jpg', 'images/10.jpg', 'images/11.jpg', 'images/12.jpg'];
+var images = ['images/1.png', 'images/2.png', 'images/3.png', 'images/4.png', 'images/5.png', 'images/6.png', 'images/7.png',
+    'images/8.png', 'images/9.png', 'images/10.png', 'images/11.png', 'images/12.png'];
 
-$("#landing").backstretch(images, { duration: 5000, fade: 750 });
+$("#landing").backstretch(images, { duration: 5000, fade: 750, scale: 'cover' });
 
 $(document).ready(function() {
 	$('#fullpage').fullpage({
